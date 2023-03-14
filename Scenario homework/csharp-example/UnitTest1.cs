9b5e0adbaf943c00e311a760e49a9ec2df651510
DaniilAstakhov <127883653+DaniilAstakhov@users.noreply.github.com>
2023-03-15 00:47:56 +0300

    Create UnitTest1.cs

---
+++ Scenario homework/csharp-example/UnitTest1.cs
@@ -0,0 +1,41 @@
+using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://localhost:8080/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            Thread.Sleep(5000);
+
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
