fd7ff845eb74804a1d6b2034f3afec82f38bc7c7
DaniilAstakhov <127883653+DaniilAstakhov@users.noreply.github.com>
2023-03-28 19:43:31 +0300

    правки

--- Scenario homework/csharp-example/UserRegistration.cs
+++ Scenario homework/csharp-example/UserRegistration.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Assert = NUnit.Framework.Assert;
 
 namespace csharp_example
 {
+    [TestFixture]
     internal class UserRegistration
     {
+
+
+        public IWebDriver CHdriver;
+        public WebDriverWait wait;
+        public ChromeOptions options;
+
+        [OneTimeSetUp]
+        public void StartBrowser()
+        {
+            options = new ChromeOptions();
+            options.AddArguments(/*"--start-maximized", */"incognito"/*, "headless"*/);
+            CHdriver = new ChromeDriver(options);
+            wait = new WebDriverWait(CHdriver, TimeSpan.FromSeconds(10));
+            CHdriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+
+        [Test]
+        public void UserRegistrationTest()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void stop()
+        {
+            CHdriver.Quit();
+        }
     }
 }
